3974cc866e16c8f253d53b20e00140ac44201970
karenarcoverde <karenarcoverde@poli.ufrj.br>
2022-01-18 16:32:53 -0300

    ajeitado a versao do python, adicionado o migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from app.config import Config
-from app.extensions import db
+from app.extensions import db, migrate
 
 from app.usuario.model import Usuario
 from app.carros.model import Carros
@@ -10,10 +10,16 @@ from app.motos.model import Motos
 from app.motosCompra.model import MotosCompra
 from app.requisicoes.model import Requisicoes
 
+# cria o app
 def create_app():
+    # instanciacao do aplicativo
     app = Flask(__name__)
+
+    # configuracao do app
     app.config.from_object(Config)
 
+    # inicializacao da database
     db.init_app(app)
+    migrate.init_app(app,db)
 
     return app
--- app/association.py
+++ app/association.py
@@ -1,9 +1,9 @@
 from app.extensions import db
 
 association_requisicoes_carros_compra = db.Table('association_requisicoes_carros_compra',db.Model.metadata,
-                                        db.Column('carros_compra',db.Integer, db.ForeignKey('carros_compra.id')),
+                                        db.Column('carroscompra',db.Integer, db.ForeignKey('carroscompra.id')),
                                         db.Column('requisicoes',db.Integer,db.ForeignKey('requisicoes.id')))
 
 association_requisicoes_motos_compra = db.Table('association_requisicoes_motos_compra',db.Model.metadata,
-                                        db.Column('motos_compra',db.Integer, db.ForeignKey('motos_compra.id')),
+                                        db.Column('motoscompra',db.Integer, db.ForeignKey('motoscompra.id')),
                                         db.Column('requisicoes',db.Integer,db.ForeignKey('requisicoes.id')))
